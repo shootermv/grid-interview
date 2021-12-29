a24c7de754da7e86eb98b76edd6dd5c415aa6740
moshe vilner <shootermv@gmail.com>
2021-12-29 14:22:00 +0200

    implementing Grid and adding support for custom component cell

--- src/Grid.js
+++ src/Grid.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Grid = ({ config, data }) => (
   <table>
     <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
+      <tr>
+        {config.map(({title}) => (
+          <th key={title}>{title}</th>
+        ))}
+      </tr>
     </thead>
     <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+      {data.map((row) => (
+        <tr key={row[config[0].field]}>
+          {config.map(({field, component: C }) => (
+            <td key={field}>{C  ? <C data={row[field]}/> : row[field]}</td> 
+          ))}
+        </tr>
+      ))}
     </tbody>
   </table>
 );
